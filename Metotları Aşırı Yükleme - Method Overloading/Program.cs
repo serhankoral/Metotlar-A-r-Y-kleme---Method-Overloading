00f1c1ce22dd5098c76d4d275316ed2a997eb492
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-12 15:56:35 +0300

    Proje dosyası ekle.

---
+++ Metotları Aşırı Yükleme - Method Overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Metotları_Aşırı_Yükleme___Method_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
