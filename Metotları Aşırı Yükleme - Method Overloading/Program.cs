39e8e21dd344ebc1b9a21d2bfa6298bbd5d6c925
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-12 16:09:36 +0300

    .

--- Metotları Aşırı Yükleme - Method Overloading/Program.cs
+++ Metotları Aşırı Yükleme - Method Overloading/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Metotları_Aşırı_Yükleme___Method_Overloading
 {
-    internal class Program
+    class Program
     {
+        /*
+         * Overloading bir fonksiyonu farklı parametreler ile yeniden yazma demektir
+         *constructor larda da kullanılır class için birden fazla oluşturma parametresi kullanılır 
+         *bir fonksiyon tanımlanır ike static tanımlanmalı ki işlem blogu içerisinde kullanılabilsin
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(topla(5, 4));
+            Console.WriteLine(topla(5, 4, 5));
+            Console.WriteLine(topla(5.2,4));
+
         }
+        static int topla(int a, int b)
+        { return a + b; }
+        static int topla(int a, int b, int c)
+        { return a + b + c; }
+
+        static double topla(double a, double b)
+        { return a+b;}
+
     }
 }
